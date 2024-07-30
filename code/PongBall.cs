4ad40cdbc6bef696c08c78d04d02deccc7300300
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2024-07-29 19:57:54 -0700

    collision is hard

--- code/PongBall.cs
+++ code/PongBall.cs
@@ -1,9 +1,34 @@
+using System;
 using Sandbox;
 
-public sealed class PongBall : Component
+public sealed class PongBall : Component, Component.ICollisionListener
 {
+	[Property] int Speed = 650;
+	Vector2 Velocity;
+
+	protected override void OnStart()
+	{
+		Velocity = Vector2.Random.Normal * Speed;
+	}
 	protected override void OnFixedUpdate()
 	{
-		Transform.Position += new Vector3(-1, 0, 0);
+		//apply velocity
+		Transform.Position += new Vector3(Velocity.x, Velocity.y, 0) * Time.Delta;
+	}
+
+	void ICollisionListener.OnCollisionStart(Collision other)
+	{
+		//reverse velocity
+		Velocity = -Velocity;
+	}
+
+	void PointScored(Connection Player)
+	{
+
+	}
+
+	void StartGame()
+	{
+
 	}
 }
